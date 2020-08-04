48dbafec1f6d7af56193f5623d86233befe709e8
shreymo12 <69166349+shreymo12@users.noreply.github.com>
2020-08-04 13:45:21 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -18,17 +18,19 @@ function setup() {
 function draw() {
   background(0); 
   if(wall.x-car.x<(car.width/2+wall.width/2)){
-       
+       car.velocityX=0;
        var deformtion = 0.5*weight*speed*speed/22509;
-       if(deforamtion<100){
-         car.shapeColor=color(0,255,0);
-       }
-       else if(deformation>100&&deformation<180){
+       if(deformtion>180){
+        car.shapeColor=color(255,0,0);
+      }
+       
+       if(deformation>100&&deformation<180){
          car.shapeColor=color(230,230,0);
        }
-       else if(deformtion>180){
-         car.shapeColor=color(255,0,0);
-       }
+       
+       if(deforamtion<100){
+        car.shapeColor=color(0,255,0);
+      }
 
   }
   
